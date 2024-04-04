12c665c3486dfb56623a9142c5d64a8faf3b1518
kimdongyoung <wwweric12@naver.com>
2024-04-04 23:00:21 +0900

    feat: middle-button 구현

--- src/components/Button/MiddleButton.tsx
+++ src/components/Button/MiddleButton.tsx
@@ -1,13 +1,7 @@
 import styled from 'styled-components';
+import { ButtonProps } from './LargeButton';
 
-export interface ButtonProps {
-  type?: 'submit' | 'button';
-  children: string;
-  onClick: () => void;
-  state: boolean;
-}
-
-const LargeButton = ({ type, children, onClick, state }: ButtonProps) => {
+const MiddleButton = ({ type, children, onClick, state }: ButtonProps) => {
   return (
     <Button type={type} onClick={onClick} $isActive={state}>
       {children}
@@ -15,19 +9,20 @@ const LargeButton = ({ type, children, onClick, state }: ButtonProps) => {
   );
 };
 
-export default LargeButton;
+export default MiddleButton;
 
 const Button = styled.button<{ $isActive: boolean }>`
-  width: 280px;
-  height: 48px;
+  width: 120px;
+  height: 36px;
   display: flex;
   justify-content: center;
   align-items: center;
-  color: white;
+  color: ${({ $isActive, theme }) =>
+    $isActive ? 'white' : theme.colors.gray2};
   background-color: ${({ $isActive, theme }) =>
     $isActive ? theme.colors.choral : theme.colors.dark_gray3};
-  font-size: 19px;
+  font-size: 14px;
   font-weight: 700;
-  border-radius: 10px;
+  border-radius: 8px;
   border: none;
 `;
---
+++ src/stories/MiddleButton.stories.ts
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import MiddleButton from '../components/Button/MiddleButton';
+
+const meta: Meta<typeof MiddleButton> = {
+  title: 'Components/MiddleButton',
+  component: MiddleButton,
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    type: 'button',
+    children: 'button',
+    onClick: () => {
+      console.log('click');
+    },
+    state: true,
+  },
+};
