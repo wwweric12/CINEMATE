65de0002d9a1ff2965a87466f8d71d6f0a3997df
kimdongyoung <wwweric12@naver.com>
2024-04-04 23:01:40 +0900

    fix: storybook title 수정

--- src/stories/LargeButton.stories.ts
+++ src/stories/LargeButton.stories.ts
@@ -2,14 +2,14 @@ import type { Meta, StoryObj } from '@storybook/react';
 import LargeButton from '../components/Button/LargeButton';
 
 const meta: Meta<typeof LargeButton> = {
-  title: 'Components/LargeButton',
+  title: 'Components/Button',
   component: LargeButton,
 };
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+export const LargeBtn: Story = {
   args: {
     type: 'button',
     children: '회원가입',
--- src/stories/MiddleButton.stories.ts
+++ src/stories/MiddleButton.stories.ts
@@ -2,14 +2,14 @@ import type { Meta, StoryObj } from '@storybook/react';
 import MiddleButton from '../components/Button/MiddleButton';
 
 const meta: Meta<typeof MiddleButton> = {
-  title: 'Components/MiddleButton',
+  title: 'Components/Button',
   component: MiddleButton,
 };
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+export const MiddleBtn: Story = {
   args: {
     type: 'button',
     children: 'button',
