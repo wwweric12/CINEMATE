3985d1c8c890dba7d2079fc725a6cb055e4fed9e
kimdongyoung <wwweric12@naver.com>
2024-04-06 16:12:54 +0900

    mypage-button-component 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import MypageButton from './components/Button/MypageButton';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <MypageButton
+        count={1}
+        onClick={() => console.log(1)}
+        content="asd"
+      ></MypageButton>
+    </>
+  );
 }
 
 export default App;
