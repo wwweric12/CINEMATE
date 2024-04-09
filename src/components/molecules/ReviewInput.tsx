372385f38f24b6eb393085483e48714591f757ee
kimdongyoung <wwweric12@naver.com>
2024-04-10 00:06:17 +0900

    feat: review-input-component 구현

---
+++ src/components/molecules/ReviewInput.tsx
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+import InputLength from '../atoms/InputLength';
+
+interface ReviewInput {
+  maxLength: number;
+  onChange: () => void;
+  value: string;
+  length: number;
+  placeholder: string;
+}
+
+const ReviewInput = ({
+  maxLength,
+  onChange,
+  value,
+  length,
+  placeholder,
+}: ReviewInput) => {
+  return (
+    <ReviewContainer>
+      <ReviewField
+        maxLength={maxLength}
+        onChange={onChange}
+        value={value}
+        placeholder={placeholder}
+      />
+      <LengthContainer>
+        <InputLength maxLength={maxLength} length={length} />
+      </LengthContainer>
+    </ReviewContainer>
+  );
+};
+
+export default ReviewInput;
+
+const ReviewContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  padding: 18px 16px;
+  background-color: ${({ theme }) => theme.colors.dark_gray4};
+  border-radius: 14px;
+`;
+
+const ReviewField = styled.textarea`
+  width: 100%;
+  height: 146px;
+  font-size: 16px;
+  text-align: left;
+  color: white;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const LengthContainer = styled.div`
+  display: flex;
+  justify-content: end;
+`;
---
+++ src/stories/ReviewInput.stories.ts
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+import ReviewInput from '../components/molecules/ReviewInput';
+
+const meta: Meta<typeof ReviewInput> = {
+  title: 'Components/Input',
+  component: ReviewInput,
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Revinput: Story = {
+  args: {
+    maxLength: 999,
+    length: 0,
+    placeholder: '리뷰를 작성해주세요',
+    onChange: fn(),
+  },
+};
