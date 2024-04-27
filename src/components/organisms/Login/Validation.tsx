04ebe1a55b0000badf01bffd2e2769c59694754c
kimdongyoung <wwweric12@naver.com>
2024-04-27 20:36:54 +0900

    fix: 코드리뷰 반영

--- src/components/organisms/Login/Validation.tsx
+++ src/components/organisms/Login/Validation.tsx
@@ -1,14 +1,11 @@
 import * as yup from 'yup';
 export const validation = yup.object().shape({
-  email: yup
-    .string()
-    .required('이메일을 다시입력해주세요')
-    .email('이메일 형식을 맞춰서 입력해주세요.'),
+  email: yup.string().required('이메일을 입력해주세요'),
   password: yup
     .string()
+    .required('비밀번호를 입력해주세요')
     .min(4, '비밀번호를 4~16자로 입력해주세요.')
     .max(16, '비밀번호를 4~16자로 입력해주세요.')
-    .required('비밀번호를 다시입력해주세요')
     .matches(
       /^(?=.*[a-zA-Z0-9]).{4,16}$/,
       '비밀번호에 영문, 숫자, 특수문자를 포함해주세요.',
