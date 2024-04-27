4a7a8fd2e830831c45c19e924bd4234ade82c1be
kimdongyoung <wwweric12@naver.com>
2024-04-27 18:24:29 +0900

    feat: signup-form 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainLayout from './layout/MainLayout';
 import LoginTemplate from './components/template/LoginTemplate';
+import SignupForm from './components/organisms/Signup/SignupForm';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
         <Route element={<MainLayout />}>
           <Route path="/login" element={<LoginTemplate />} />
           <Route path="/" />
-          <Route path="/signup" />
+          <Route path="/signup" element={<SignupForm />} />
           <Route path="/success" />
         </Route>
       </Routes>
